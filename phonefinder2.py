1c9b0228835397a31d1d7a8a0b895a826ad8d717
m3lu <139358200+m3lu@users.noreply.github.com>
2023-08-05 09:36:15 -0700

    Update phonefinder2.py

--- phonefinder2.py
+++ phonefinder2.py
@@ -4,7 +4,6 @@ from phonenumbers import geocoder
 
 number = input("Enter the number ")
 
-file.write(name)
 ch_number = phonenumbers.parse(number, "CH")
 print(geocoder.description_for_number(ch_number, "en"))
 
